8306dce9eb591167e1c01ae7f49870927debf8ed
Syamiera Hani <syamierahani@gmail.com>
2026-01-14 18:04:32 +0800

    Change View Artist's layout schema and field

--- app/Filament/Resources/Artists/Schemas/ArtistInfolist.php
+++ app/Filament/Resources/Artists/Schemas/ArtistInfolist.php
@@ -3,8 +3,10 @@
 namespace App\Filament\Resources\Artists\Schemas;
 
 use App\Models\Artist;
-use Filament\Infolists\Components\TextEntry;
 use Filament\Schemas\Schema;
+use Filament\Schemas\Components\Fieldset;
+use Filament\Infolists\Components\TextEntry;
+use Filament\Infolists\Components\ImageEntry;
 
 class ArtistInfolist
 {
@@ -12,25 +14,34 @@ class ArtistInfolist
     {
         return $schema
             ->components([
-                TextEntry::make('name'),
-                TextEntry::make('bio')
-                    ->placeholder('-')
-                    ->columnSpanFull(),
-                TextEntry::make('email')
-                    ->label('Email address'),
-                TextEntry::make('contact')
-                    ->placeholder('-'),
-                TextEntry::make('picture')
-                    ->placeholder('-'),
-                TextEntry::make('created_at')
-                    ->dateTime()
-                    ->placeholder('-'),
-                TextEntry::make('updated_at')
-                    ->dateTime()
-                    ->placeholder('-'),
-                TextEntry::make('deleted_at')
-                    ->dateTime()
-                    ->visible(fn (Artist $record): bool => $record->trashed()),
+                Fieldset::make('Basic Info')
+                    ->columns([
+                        'default' => 1,
+                        'md' => 2,
+                        'xl' => 3,
+                    ])
+                    ->schema([
+                        TextEntry::make('name'),
+                        TextEntry::make('bio')->placeholder('-')->columnSpanFull(),
+                    ]),
+
+                Fieldset::make('Contact Info')
+                    ->columns(2)
+                    ->schema([
+                        TextEntry::make('email')->label('Email address'),
+                        TextEntry::make('contact')->placeholder('-'),
+                    ]),
+
+                Fieldset::make('Media & Timestamps')
+                    ->columns(3)
+                    ->schema([
+                        ImageEntry::make('picture')->placeholder('-'),
+                        TextEntry::make('created_at')->dateTime()->placeholder('-'),
+                        TextEntry::make('updated_at')->dateTime()->placeholder('-'),
+                        TextEntry::make('deleted_at')
+                            ->dateTime()
+                            ->visible(fn (Artist $record): bool => $record->trashed()),
+                    ]),
             ]);
     }
 }
