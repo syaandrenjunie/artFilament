ef090103160077dcd65e3d8a1cd1d7e12bef1be5
Syamiera Hani <syamierahani@gmail.com>
2026-01-14 18:11:15 +0800

    Create schema for CategoryForm

--- app/Filament/Resources/Categories/Schemas/CategoryForm.php
+++ app/Filament/Resources/Categories/Schemas/CategoryForm.php
@@ -3,6 +3,9 @@
 namespace App\Filament\Resources\Categories\Schemas;
 
 use Filament\Schemas\Schema;
+use Filament\Schemas\Components\Fieldset;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
 
 class CategoryForm
 {
@@ -10,7 +13,20 @@ class CategoryForm
     {
         return $schema
             ->components([
-                //
+                Fieldset::make('Category Information')
+                    ->columns([
+                        'default' => 1,
+                        'md' => 2,
+                        'xl' => 3,
+                    ])
+                    ->schema([
+                        TextInput::make('name')
+                            ->required(),
+
+                        Textarea::make('description')
+                            ->columnSpanFull(),
+                    ]),
             ]);
     }
 }
+
