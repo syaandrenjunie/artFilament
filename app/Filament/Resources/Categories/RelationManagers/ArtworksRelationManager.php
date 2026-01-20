aba8b547b5e88f0dd00cf16866a78f51a18613f2
Syamiera Hani <syamierahani@gmail.com>
2026-01-20 21:48:42 +0800

    Add some comments about each line in ArtworksRelationManager.php

--- app/Filament/Resources/Categories/RelationManagers/ArtworksRelationManager.php
+++ app/Filament/Resources/Categories/RelationManagers/ArtworksRelationManager.php
@@ -9,15 +9,18 @@ use Filament\Tables\Table;
 
 class ArtworksRelationManager extends RelationManager
 {
-    protected static string $relationship = 'artworks';
+    protected static string $relationship = 'artworks'; 
+    //name of the relationship method in Category model
 
     protected static ?string $relatedResource = ArtworkResource::class;
+    //reuse whatever is defined in ArtworkResource - table / form / infolist
+    //when enabled YES to linking
 
     public function table(Table $table): Table
     {
         return $table
             ->headerActions([
-                CreateAction::make(),
+                CreateAction::make(),   //trgigger the ArtworkResource form
             ]);
     }
 }
